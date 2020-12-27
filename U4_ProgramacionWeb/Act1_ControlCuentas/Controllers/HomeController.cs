12244c28e3dc374885a7c632f851270d274fb829
Jessica <171g0214@rcarbonifera.tecnm.mx>
2020-12-27 16:12:10 -0600

    Diseño de interfaz

---
+++ U4_ProgramacionWeb/Act1_ControlCuentas/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Act1_ControlCuentas.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        [Authorize(Roles ="Seguidor")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult IniciarSesion()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult IniciarSesion( string username, string password)
+        {
+            return View();
+        }
+
+    }
+}
