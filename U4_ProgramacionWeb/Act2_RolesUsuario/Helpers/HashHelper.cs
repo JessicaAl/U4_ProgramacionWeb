ce27196e4c43b12535c64b28fb6f9e3bc2dc281d
Jessica <171g0214@rcarbonifera.tecnm.mx>
2021-01-05 03:55:23 -0600

    Clases + Views

---
+++ U4_ProgramacionWeb/Act2_RolesUsuario/Helpers/HashHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Act2_RolesUsuario.Helpers
+{
+    public class HashHelper
+    {
+        public static string GetHash(string cadena)
+        {
+            var sha = SHA256.Create();
+            byte[] codificar = Encoding.UTF8.GetBytes(cadena);
+            byte[] hash = sha.ComputeHash(codificar);
+            string a = "";
+            foreach (var objeto in hash)
+            {
+                a += objeto.ToString("x2");
+            }
+            return a;
+        }
+    }
+}
---
+++ U4_ProgramacionWeb/Act2_RolesUsuario/Models/ViewModels/AlumnoViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Act2_RolesUsuario.Models.ViewModels
+{
+    public class AlumnoViewModel
+    {
+        public Alumno Alumno { get; set; }
+        public Maestro Maestro { get; set; }
+        public IEnumerable<Maestro> Maestros { get; set; }
+    }
+}
